f6ce7e6e5a78e89f0399029f3518c4f87a5e23da
Alyssa A <165829184+aalmond3@users.noreply.github.com>
2025-03-20 02:15:19 +0000

    js and cssc

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log('OK');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const stars = document.querySelectorAll("#stars path");
+
+    stars.forEach(star => {
+        function twinkle() {
+            const duration = Math.random() * 1 + 1.5; 
+            const delay = Math.random() * .5; 
+
+            star.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+            star.style.opacity = Math.random() * 1 + 0; 
+
+            setTimeout(twinkle, duration * 1000 + delay * 1000); 
+        }
+        twinkle();
+    });
+});
